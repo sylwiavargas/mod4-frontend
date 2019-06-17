23a40bbafef7ef739c083d66f02be6244c16aa9c
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-06-17 12:26:07 -0400

    first state

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import '../App.css';
 import NavBarContainer from './NavBarContainer.js';
 import MainContainer from './MainContainer.js';
-import ReportForm from '../components/ReportForm'
-
 
 class App extends Component {
 
+  state = {
+    logged: true
+  }
+
   render() {
+    // console.log(this.state)
     return (
       <div>
-      <NavBarContainer/>
+      <NavBarContainer logged={this.state.logged}/>
       <MainContainer/>
       </div>
     )
