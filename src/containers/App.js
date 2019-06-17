7951f0252234bf45015e935c324502fde1f84fb9
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-06-17 12:31:19 -0400

    added login function

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,11 +9,17 @@ class App extends Component {
     logged: true
   }
 
+  changeLogin = () => {
+    return this.setState(prevState => {
+      return {logged: !prevState}
+    })
+  }
+
   render() {
     // console.log(this.state)
     return (
       <div>
-      <NavBarContainer logged={this.state.logged}/>
+      <NavBarContainer logged={this.state.logged} changeLogin={this.changeLogin}/>
       <MainContainer logged={this.state.logged}/>
       </div>
     )
