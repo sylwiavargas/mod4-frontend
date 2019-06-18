a81ecc5e80c58697b7ee80f7ad3713425512b5ed
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-06-18 16:23:24 -0400

    working on css

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,7 +36,7 @@ class App extends Component {
   }
 
   createStyles = () => {
-    let classNames = ""
+    let classNames = "thing "
     classNames += this.state.dark ? "dark" : "light"
     classNames += this.state.sizeUp ? " bigText" : " smallText"
     classNames += this.state.fontReadable ? " readableFont" : " fancyFont"
@@ -47,19 +47,19 @@ class App extends Component {
     // console.log(this.state)
     return (
       <div className={this.createStyles()}>
-      <NavBarContainer
-        logged={this.state.logged}
-        changeLogin={this.changeLogin}
-        saveData={this.saveData}
-        userName={this.state.userName}
-        caseStudies={this.state.caseStudies}
-        changeSetting={this.changeSetting}
-        dark={this.state.dark}
-        sizeUp={this.state.sizeUp}
-        fontReadable={this.state.fontReadable}/>
-      <MainContainer
-        logged={this.state.logged}
-        caseStudies={this.state.caseStudies}/>
+        <NavBarContainer
+          logged={this.state.logged}
+          changeLogin={this.changeLogin}
+          saveData={this.saveData}
+          userName={this.state.userName}
+          caseStudies={this.state.caseStudies}
+          changeSetting={this.changeSetting}
+          dark={this.state.dark}
+          sizeUp={this.state.sizeUp}
+          fontReadable={this.state.fontReadable}/>
+        <MainContainer
+          logged={this.state.logged}
+          caseStudies={this.state.caseStudies}/>
       </div>
     )
   }
